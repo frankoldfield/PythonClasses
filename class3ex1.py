1a10eccb0116929fe94012949f4b65c675a56c2a
Frank Oldfield <48333349+frankoldfield@users.noreply.github.com>
2022-09-29 19:14:48 +0200

    Primer ejercicio de la tercera semana, una función que crea un fichero csv con nombres y números de teléfono aleatorios, con el nombre de fichero que tu le des, y el número de entradas. Coge una lista determinada en la función, pero se podría cambiar

---
+++ class3ex1.py
@@ -0,0 +1,16 @@
+import random
+
+def CreaAgenda(nombreFichero, n):
+    lista_nombres = ['Antonio', 'Alfonso', 'Eugenio', 'Evaristo', 'Pablo', 'Alberto', 'Alejandro', 'Laura', 'Estela', 'Rosa']
+    lista_apellidos = ['Martínez', 'Morales', 'Marín', 'López', 'García', 'Zapata', 'Gómez', 'Ruíz', 'Sánchez', 'Chicote']
+    archivo = open(nombreFichero+'.csv', 'w', encoding='utf8')
+    ini = 100000000
+    fin = 999999999
+    for i in range (0,n,1):
+        entrada = lista_nombres[random.randint(0, 9)]+' '
+        entrada += lista_apellidos[random.randint(0, 9)]+' '
+        entrada += lista_apellidos[random.randint(0, 9)]
+        entrada += ';'+str(random.randint(ini,fin)) #Importante hacer el casting, si no no puede concatenar con el +=
+        archivo.write('<'+entrada+'>\n') #Normálmente se pueden concatenar Strings con la coma, pero como .write es una función
+CreaAgenda('Prueba', 7)                #que toma parámetros, la , se toma como una separación de parámetros, asi que tenemos que concatenarlos con el +
+#Puede ser que la función que crea un string tome varios parámetros separados con la , y los concatena, y en realidad eso es lo que está pasando cuando concatenamos con las comas?
