9d371d55142f30e059fcacc2becc28a70966131f
Frank Oldfield <48333349+frankoldfield@users.noreply.github.com>
2022-10-06 12:41:43 +0200

    Cambio nombre de diccionario a agenda

--- class3ex2.py
+++ class3ex2.py
@@ -2,12 +2,12 @@ def leeAgenda(NombreFichero):
     archivo = open(NombreFichero,'r')
     lineas = archivo.readlines()
     n=1
-    diccionario = {}
+    agenda = {}
     for linea in lineas:
         nuevaentrada = linea.strip('<>\n') #Aquí le quitamos los caracteres indicados
         nuevaentrada = nuevaentrada.split(sep=';') #Aquí dividimos los campos a introducir en el diccionario
-        diccionario[nuevaentrada[0]]=(nuevaentrada[1:]) #Como split devuelve una lista de cadenas, tenemos que coger la posición 0 para el nombre y las demás para los números
+        agenda[nuevaentrada[0]]=(nuevaentrada[1:]) #Como split devuelve una lista de cadenas, tenemos que coger la posición 0 para el nombre y las demás para los números
         #print(diccionario['Alejandro Ruíz Sánchez']) #Este print son para comprobar que se han introducido bien los datos
         n += 1
-    return diccionario
+    return agenda
 leeAgenda('Prueba.csv')
