6a623a51161c5ccf18a1153407cdbf0cd815605e
Frank Oldfield <48333349+frankoldfield@users.noreply.github.com>
2022-10-06 12:52:47 +0200

    Versión del profesor

--- class3ex2.py
+++ class3ex2.py
@@ -1,12 +1,22 @@
 def leeAgenda(NombreFichero):
-    archivo = open(NombreFichero,'r')
+    archivo = open(NombreFichero,'rt')
     lineas = archivo.readlines()
     n=1
     agenda = {}
-    for linea in lineas:
-        nuevaentrada = linea.strip('<>\n') #Aquí le quitamos los caracteres indicados
-        nuevaentrada = nuevaentrada.split(sep=';') #Aquí dividimos los campos a introducir en el diccionario
-        agenda[nuevaentrada[0]]=(nuevaentrada[1:]) #Como split devuelve una lista de cadenas, tenemos que coger la posición 0 para el nombre y las demás para los números
+    while l:
+        campos = l.split(";")
+        nombre = campos[0]
+        tlf = campos[1].strip()
+        if not agenda.get(nombre):
+            agenda[nombre]=[tlf]
+        else
+            agenda[nombre].append(tlf)
+            print()
+        archivo=readline.f
+        archivo.close()
+        #nuevaentrada = nuevaentrada.split(sep=';') #Aquí dividimos los campos a introducir en el diccionario
+        #nuevaentrada = nuevaentrada.split(sep=';')
+        #agenda[nuevaentrada[0]]=(nuevaentrada[1:]) #Como split devuelve una lista de cadenas, tenemos que coger la posición 0 para el nombre y las demás para los números
         #print(diccionario['Alejandro Ruíz Sánchez']) #Este print son para comprobar que se han introducido bien los datos
         n += 1
     return agenda
